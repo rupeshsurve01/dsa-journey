a0e4d7b31e31afb1898c413931be7a86a66bf41c
Rupesh Surve. <rupeshsurve302@gmail.com>
2026-06-22 23:12:39 +0530

    Time: 2 ms (99.93%), Space: 52.8 MB (20.93%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,23 +3,40 @@ class Solution {
 
         List<Integer> list = new ArrayList<>();
         int n = nums.length;
+        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
+        int cnt1 = 0, cnt2 = 0;
 
         for (int i = 0; i < nums.length; i++) {
 
-            if (list.size() == 0 || list.get(0) != nums[i] && (list.size() < 2 || list.get(1) != nums[i])) {
-
-                int cnt = 0;
-
-                for (int j = i; j < nums.length; j++) {
-
-                    if (nums[i] == nums[j])
-                        cnt++;
-                }
-
-            if (cnt > (n / 3))
-                list.add(nums[i]);
-            }
+           if(cnt1 == 0 && el2 != nums[i]){
+            el1 = nums[i];
+            cnt1++;
+           }
+           else if(cnt2 == 0&& el1 != nums[i]){
+            el2 = nums[i];
+            cnt2++;
+           }
+           else if(nums[i] == el1) cnt1++;
+           else if(nums[i] == el2) cnt2++;
+           else{
+            cnt1--;
+            cnt2--;
+           }
         }
+
+
+        cnt1 = 0;
+         cnt2 = 0;
+
+        for(int i = 0;i < n; i++){
+            if(el1 == nums[i])cnt1++;
+            if(el2 == nums[i])cnt2++;
+        }
+
+        int mini = n/3+1;
+
+        if(cnt1 >= mini) list.add(el1);
+        if(cnt2 >= mini) list.add(el2);
 
         return list;
     }
